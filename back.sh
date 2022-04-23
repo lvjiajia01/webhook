a465434b09440e4b657b194c6d02f45fcdc76441
lv <lvjiajia@fofund.com.cn>
2022-04-23 22:18:17 +0800

    update

--- back.sh
+++ back.sh
@@ -7,7 +7,7 @@ git clean -f
 echo "拉取最新代码"
 git pull origin master
 echo "开始执行构建"
-docker build -t back .
+docker build -t back:1.0 .
 echo "停止旧容器并删除旧容器"
 docker stop back-container
 docker rm back-container
